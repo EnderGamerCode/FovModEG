200bdf4f0f3e7622d7413c7f0a94def19067faf8
EnderGamerCode <88829642+EnderGamerCode@users.noreply.github.com>
2022-11-25 16:08:54 +0100

    FovMod

---
+++ src - Kopie/main/java/endergamer/ModSpecs.java
@@ -0,0 +1,9 @@
+package endergamer;
+
+public class ModSpecs {
+
+    public static final String MODID = "em",
+                               NAME = "EnderMod",
+                               VERSION = "1.0";
+
+}
